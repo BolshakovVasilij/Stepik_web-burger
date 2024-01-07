d8c689961c22172cc551f97cc9757b9c18e92efc
Bolshakov Vasilij <bovanikoz@gmail.com>
2024-01-07 21:57:32 +0300

    Завершить Header и Footer

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+const menuBtn = document.querySelector ('.footer__menu-btn'); 
+const menuMobile = document.querySelector ('.footer__list');
+ menuBtn.addEventListener ('click', ()=> {menuMobile.classList.toggle ('footer-list--open')
+ });
+ const conditionMobile = document.querySelector ('.footer__conditions');
+ menuBtn.addEventListener ('click', ()=> {conditionMobile.classList.toggle ('footer__conditions--open')
+ });
+
+ const navBtn = document.querySelector('.nav-btn');
+ const navMobile = document.querySelector('.player-nav__list');
+ navBtn.addEventListener ('click', ()=> {navMobile.classList.toggle ('player-nav__list--open')
+});
+// const closeBtn = document.querySelector ('.close-nav__cross');
+// closeBtn.addEventListener('click',()=>{navMobile.classList.remove('player-nav__list--open')
+//});
